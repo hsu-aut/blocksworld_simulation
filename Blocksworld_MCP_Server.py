f4a217ca56232500f31ab934c79b49b8c9625d6c
Niklas Jobs <niklas.jobs@hsu-hh.de>
2025-07-21 18:21:50 +0200

    Update Blocksworld_MCP_Server.py
    Better description of the tools

--- Blocksworld_MCP_Server.py
+++ Blocksworld_MCP_Server.py
@@ -1,3 +1,4 @@
+# Blocksworld_Robot_Actions_Server.py
 from mcp.server.fastmcp import FastMCP
 import requests
 
@@ -9,10 +10,10 @@ SIM_API = "http://localhost:5001"  # URL of the Blocksworld simulation API
 def pick_up(block:str) -> str:
     """
     Description:
-        Picks up a block that is lying on the ground.
+        Picks up a block that is lying directly on the ground.
 
-    Prerequisite: 
-        The block must not be stacked on another block 
+    Requirements: 
+        The block to be picked up must be on the ground (= block is in the first position of the stack).
         There must not be any other block on top of it.
 
     Result:
@@ -29,14 +30,37 @@ def pick_up(block:str) -> str:
 
 
 @mcp.tool()
+def unstack(block1:str, block2:str) -> str: 
+    """
+    Description:
+        Unstacks block1 from block2.
+
+    Requirements:
+        The robot must hold block1.
+        block2 must have block1 on top of it.         
+    Result:
+        block1 will be unstacked from block2 and the robot will hold it.
+
+    Args:
+        block1 (str): The name of the block to unstack.
+        block2 (str): The name of the block from which to unstack.
+    """
+    try:
+        r = requests.post(f"{SIM_API}/unstack", json={"block1": block1, "block2": block2}, timeout=2)
+        return r.json().get("result", f"Requested unstack {block1} from {block2}")
+    except Exception as e:
+        return f"[ERROR]: {str(e)}"
+    
+
+@mcp.tool()
 def put_down(block:str) -> str:
     """
     Description:
-        Puts down a held block on the ground.
+        Puts down a held block directly on the ground on a free stack.
 
-    Prerequisite:
-        The robot must hold the block
-        There must be a free space on the ground.
+    Requirements:
+        There must be a free space on the ground (= One stack has to be empty).
+        The robot must hold the block.
 
     Result:
         The block will be placed on the ground.
@@ -58,7 +82,7 @@ def stack(block1:str, block2:str) -> str:
     Description:
         Stacks block1 on top of block2.
 
-    Prerequisite    
+    Requirements:    
         The robot must hold block1.
         Block2 must not have any block on top of it.
 
@@ -74,29 +98,7 @@ def stack(block1:str, block2:str) -> str:
         return r.json().get("result", f"Requested stack {block1} on {block2}")
     except Exception as e:
         return f"[ERROR]: {str(e)}" 
-
-
-@mcp.tool()
-def unstack(block1:str, block2:str) -> str: 
-    """
-    Description:
-        Unstacks block1 from block2.
-
-    Prerequisite:
-        The robot must hold block1.
-        Block2 must have block1 on top of it.         
-    Result:
-        Block1 will be unstacked from block2 and the robot will hold it.
-
-    Args:
-        block1 (str): The name of the block to unstack.
-        block2 (str): The name of the block from which to unstack.
-    """
-    try:
-        r = requests.post(f"{SIM_API}/unstack", json={"block1": block1, "block2": block2}, timeout=2)
-        return r.json().get("result", f"Requested unstack {block1} from {block2}")
-    except Exception as e:
-        return f"[ERROR]: {str(e)}"
+    
     
 @mcp.tool()
 def get_status() -> str: 
@@ -104,8 +106,12 @@ def get_status() -> str:
     Description:
         Queries the current status of the system
      
+    Requirements:
+        None
+
     Result:
-        Returns the current status of the robot including any held blocks and the positions of the blocks.
+        Returns the current status of the robot including any held blocks and the total number of available stacks and the positions of the blocks.
+
 
     Args:
         None
