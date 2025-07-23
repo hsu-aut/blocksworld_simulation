f8126f0c71be1ff50c3ea36aa4d469e83946f74e
Miguel Vieira <miguel_vieira@gmx.de>
2025-07-23 19:13:22 +0200

    Refactor main function to simplify Flask initialization by removing threading and pygame integration

--- src/blocksworld_simulation/main.py
+++ src/blocksworld_simulation/main.py
@@ -1,13 +1,8 @@
-from blocksworld_simulation.api.api import start_flask_thread
-from blocksworld_simulation.simulation.simulation import start_pygame_mainloop
+from blocksworld_simulation.api.api import run_flask
 
 def main(): 
-    # Start Flask in a separate thread
-    start_flask_thread()
     
-    # Run pygame on the main thread (required for macOS)
-    start_pygame_mainloop()
-
+    run_flask()
 
 if __name__ == "__main__":
     main()
