bb3c1edae26dc721bf80dc6c6452f7dea85fd042
mxweigand <mxweigand@gmail.com>
2025-08-27 12:19:49 +0200

    fix: updates rules to markdown notation

--- src/blocksworld_simulation/constraints/contraint_sets/hanoi_towers/hanoi_towers_rules.py
+++ src/blocksworld_simulation/constraints/contraint_sets/hanoi_towers/hanoi_towers_rules.py
@@ -1,5 +1,5 @@
 def get_additional_rules() -> str:
-    return """HANOI TOWERS RULES
+    return """# Hanoi Towers Rules
 
 The Hanoi Towers problem is a classic puzzle involving three stacks and a number of blocks of different sizes (widths, given by the x_size property of a block) which can be placed on other stacks. 
 The puzzle starts with the blocks stacked in ascending order of size on one stack, the smallest at the top.
@@ -9,7 +9,7 @@ The objective is to move the entire stack to another stack, following these rule
 2. A block can only be placed on top of a larger block or on an empty stack.
 3. To place blocks, the actions of the basic blocksworld problem must be used (i.e. pick_up, put_down, stack, unstack).
 
-The basic blocks world rules are the following:
+The basic blocks world rules have to be obeyed, they can be found in the following section.
 
 """
 
